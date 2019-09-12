cb7bebbcff7cceed81c81bf273b86bd24a1d2744
Kento999 <54842363+Kento999@users.noreply.github.com>
2019-09-12 13:53:51 +0900

    Add files via upload
    ウェブアプリ途中経過

---
+++ recipe3/recipe_app/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.conf.urls.static import static
+from .views import MyappView
+from django.conf import settings
+
+urlpatterns = [
+              url(r'', MyappView.as_view(), name='index'),
+              ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
