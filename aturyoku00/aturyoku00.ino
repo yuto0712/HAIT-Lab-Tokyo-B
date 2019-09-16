2f68d0e5bec2421b8a6733e8a7a8a8cd9a6c9e9c
fumiyaito <fumiya.soccer.110@gmail.com>
2019-09-16 15:28:30 +0900

    modified arduino code and added django project

--- aturyoku00/aturyoku00.ino
+++ aturyoku00/aturyoku00.ino
@@ -6,16 +6,17 @@ Serial.begin(9600);
 
 void loop() { //電源が切れるまで繰り返し実行される
 val = analogRead(0); //アナログピンから値を読み取る。0はピン番号
-Serial.println(val); //シリアルポートへ出力。printlnで改行してくれる
+//Serial.println(val); //シリアルポートへ出力。printlnで改行してくれる
 delay(700); //1000ms（1秒）ごとに実行
-if (val > 170) {
-  Serial.println("High");
+if (val > 20 and val < 100) {
+  Serial.println("H");
   digitalWrite(13,HIGH);
-  delay(1000);
+  delay(500);
   }
 else  {
   digitalWrite(13,LOW);
-  Serial.println("Low");
-  delay(1000);
+  Serial.println("L");
+  Serial.println(val);
+  delay(500);
   }
-  }
+}
